6e20492aff5e14312dc3c9c41a68125676782a7c
indiasam <sam_ama@163.com>
2016-08-10 14:09:48 +0800

    do it

--- myjava/src/myjava/MyClass.java
+++ myjava/src/myjava/MyClass.java
@@ -5,19 +5,22 @@
  * Date:2016年8月10日下午1:58:54
  * Copyright (c) 2016, 浙江正元智慧科技股份有限公司 All Rights Reserved.
  *
-*/
+ */
 
 package myjava;
 
 /**
  * ClassName:MyClass <br/>
  * Function: TODO 类功能描述. <br/>
- * Date:     2016年8月10日 下午1:58:54 <br/>
- * @author   CHN
- * @version  1.0.0
- * @since    JDK 1.7
+ * Date: 2016年8月10日 下午1:58:54 <br/>
+ * 
+ * @author CHN
+ * @version 1.0.0
+ * @since JDK 1.7
  */
 public class MyClass {
 
-}
+	public static void main(String[] arg) {
 
+	}
+}
